384b988fdab3dfc42a50ebaca17a11ccfcdeae53
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2026-09-08 16:35:28 +0530

    Create symmetricDifference.js

---
+++ symmetricDifference.js
@@ -0,0 +1,23 @@
+// Symmetric Difference in sets
+<html>
+<body>
+<h1>JavaScript Sets</h1>
+<h2>The symmetricDifference() Method</h2>
+
+<p>The symmetricDifference() method returns a new set containing elements which are in this set or in the argument set, but not in both:</p>
+<p id="demo"></p>
+
+<script>
+const A = new Set(['a','b','c']);
+const B = new Set(['b','c','d']);
+
+const C = A.symmetricDifference(B);
+
+let text = "";
+for (const x of C) {
+  text += x;
+}
+document.getElementById("demo").innerHTML = "The symmetric difference is: " + text;
+</script>
+</body>
+</html>
